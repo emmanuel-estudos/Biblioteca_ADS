8d0438fe3ea0ca851b1c2bd8a609a54f306f58e0
Emmanuel (Lab06) <emmanuelleite12345@gmail.com>
2026-05-28 11:06:14 -0300

    DOCS (P5 - Segurança de Dados): organização de arquivos da disciplina

--- src/contents/periodo05/Seguranca-de-Dados/config.ts
+++ src/contents/periodo05/Seguranca-de-Dados/config.ts
@@ -6,8 +6,15 @@ export const config = {
     
     // Mapeamento para nomes amigáveis
     assuntos: {
+        "Configuracao-Ambiente-Atividade02--End-Points": "Configuração Atividade 02: End Points",
+
+        "Topico-02--Contextualizando-a-Seguranca-da-Informacao": "Tópico 02: Contextualizando a Segurança da Informação",
+        "Topico-03--Criptografia-Basica": "Tópico 03: Criptografia Básica",
+        "Topico-04--Criptografia-Simetrica": "Tópico 04: Criptografia Simétrica",
+        "Topico-05--Criptografia-de-Chave-Publica-ou-Assimetrica": "Tópico 05: Criptografia-de-Chave Pública ou Assimétrica",
+        "Topico-06--Funcoes-de-Hash-Criptograficas": "Tópico 06: Funções de Hash Criptográficas",
         "Topico-07--ConfigurandoVMs": "Tópico 07: Configurando VMs",
         "Topico-08--Criptografia": "Tópico 08: Fundamentos de Criptografia",
-        // Adicione os próximos arquivos conforme criá-los
+        // Adicione os próximos arquivos conforme forem criados
     }
 };
