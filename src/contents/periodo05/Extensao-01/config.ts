298eec43ed1768fc3973a5f1884661f2fc26d5b1
Emmanuel <emmanuelleite12345@gmail.com>
2026-07-15 23:20:26 -0300

    DOCS (Extensão_01): JavaScript Capítulo 01 (Sintaxes básicas e Variáveis)

---
+++ src/contents/periodo05/Extensao-01/config.ts
@@ -0,0 +1,27 @@
+export const config = {
+		nome: "Extensão 01",
+		periodo: "5º Período",
+		corPrimaria: '#06b6d4',
+		corSecundaria: '#0369a1',
+		
+	// Mapeamento para arquivos da aba 'Assuntos' (Materia.tsx)
+	assuntos: {
+		"Curso-JavaScript": "Instruções",
+		"JavaScript-Cap01": "JavaScript - Capítulo 01: Sintaxes Básicas e Variáveis"
+	},
+	// Mapeamento para aba de 'Atividades' (Materia.tsx)
+	atividades: {
+		"atividade01": {
+			nome: "Atividade 01",
+			arquivos: {
+				"Teste": "Teste de arquivo"
+			}
+		},
+		"atividade02": {
+			nome: "Atividade 02: Mininet",
+			arquivos: {
+				"Roteiro": "Roteiro de Execução"
+			}
+		}
+	}
+};
