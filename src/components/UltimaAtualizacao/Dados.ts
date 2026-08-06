f80c6096bd3d5459e470542e20458ef5584dfa3c
Emmanuel <emmanuelleite12345@gmail.com>
2026-08-06 00:34:08 -0300

    DOCS (UltimaAtualizacao.tsx): mensagem de atualização do site referente aos roteiros das atividades 02 e 03 de Segurança de Dados

--- src/components/UltimaAtualizacao/Dados.ts
+++ src/components/UltimaAtualizacao/Dados.ts
@@ -5,13 +5,10 @@ export interface UltimaAtualizacaoData {
 }
 
 export const ultimaAtualizacaoInfo: UltimaAtualizacaoData = {
-  mensagem: "DOCS (Segurança de Dados): Roteiro para a Atividade03",
+  mensagem: "DOCS (Segurança de Dados): Roteiros das Atividades 02 e 03",
   descricao: `
-		- Passo a passo da atividade.\n
-		- Contém um arquivo somente para as configurações iniciais.\n
-		- A atividade possui 3 passos e cada um está em um arquivo.\n
-		* Adição dos "terminais" para a ilustração de comandos.\n
-		* Link para os vídeos e as VMs necessárias para realizar a atividade.
+		- Correção de alguns pontos no roteiro da Atividade03.\n
+		- Troca de números de IPs específicos para o nome do local a que ele se refere.
 	`,
   // dataHora: "2026-07-31T16:30:00-03:00",
 };
