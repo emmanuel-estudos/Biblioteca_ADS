4737609da888fe054c8d94e840e5d60bc2292e47
Emmanuel <emmanuelleite12345@gmail.com>
2026-08-17 21:32:19 -0300

    Updates

--- assets/q002-c-BNLL9Rul.js
+++
@@ -1,5 +0,0 @@
-import{n as e,r as t}from"./lib-CrvGBhFj.js";var n=t();function r(t){let r={code:`code`,h1:`h1`,p:`p`,pre:`pre`,span:`span`,...e(),...t.components};return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)(r.h1,{id:`questão-02`,children:`Questão 02`}),`
-`,(0,n.jsx)(r.p,{children:`teste de texto comum`}),`
-`,(0,n.jsx)(r.p,{children:(0,n.jsx)(r.code,{children:`teste de texto inline-code`})}),`
-`,(0,n.jsx)(r.pre,{children:(0,n.jsxs)(r.code,{className:`hljs language-c`,children:[(0,n.jsx)(r.span,{className:`hljs-type`,children:`int`}),` numero = `,(0,n.jsx)(r.span,{className:`hljs-number`,children:`1`}),`;
-`]})})]})}function i(t={}){let{wrapper:i}={...e(),...t.components};return i?(0,n.jsx)(i,{...t,children:(0,n.jsx)(r,{...t})}):r(t)}export{i as default};
---
+++ assets/q002-c-Dx47JqIz.js
@@ -0,0 +1,6 @@
+import{n as e,r as t}from"./lib-CrvGBhFj.js";var n=t();function r(t){let r={code:`code`,h1:`h1`,h2:`h2`,p:`p`,pre:`pre`,...e(),...t.components};return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)(r.h1,{id:`questão-002`,children:`Questão 002`}),`
+`,(0,n.jsx)(r.h2,{id:`enunciado`,children:`Enunciado`}),`
+`,(0,n.jsx)(r.p,{children:`Escreva um programa que leia três números e seus respectivos pesos e calcule a sua média ponderada.`}),`
+`,(0,n.jsx)(r.h2,{id:`resolução`,children:`Resolução`}),`
+`,(0,n.jsx)(r.pre,{children:(0,n.jsx)(r.code,{className:`hljs language-C`})}),`
+`,(0,n.jsx)(r.h1,{id:`explicações`,children:`Explicações`})]})}function i(t={}){let{wrapper:i}={...e(),...t.components};return i?(0,n.jsx)(i,{...t,children:(0,n.jsx)(r,{...t})}):r(t)}export{i as default};
